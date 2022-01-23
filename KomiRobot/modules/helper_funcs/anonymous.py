9a517b97865bdc8c69036f5eec1fe193f7b1ba13
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-23 13:49:36 +0530

    Update anonymous.py

--- KomiRobot/modules/helper_funcs/anonymous.py
+++ KomiRobot/modules/helper_funcs/anonymous.py
@@ -6,7 +6,7 @@ from telegram.ext import CallbackContext
 from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
 from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 
-from KomiRobot import DEV_USERS, SUDO_USERS, dispatcher
+from KomiRobot import DEV_USERS, DRAGONS, dispatcher
 from KomiRobot.modules.helper_funcs.decorators import  siestacallback
 
 
@@ -50,7 +50,7 @@ def user_admin(permission: AdminPerms):
                 user_id = message.from_user.id
                 chat_id = message.chat.id
                 mem = context.bot.get_chat_member(chat_id=chat_id, user_id=user_id)
-                if getattr(mem, permission.value) is True or mem.status == "creator" or user_id in SUDO_USERS:
+                if getattr(mem, permission.value) is True or mem.status == "creator" or user_id in DRAGONS:
                     return func(update, context, *args, **kwargs)
                 else:
                     return message.reply_text(f"You lack the permission: `{permission.name}`",
