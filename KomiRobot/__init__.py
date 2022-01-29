c94f59c6ca0e70adb4cb6501246b5229365cdc4f
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-30 02:30:57 +0530

    Update __init__.py

--- KomiRobot/__init__.py
+++ KomiRobot/__init__.py
@@ -260,6 +260,7 @@ pgram = Client("KuramaPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 print("[INFO]: INITIALIZING AIOHTTP SESSION")
 # ARQ Client
+aiohttpsession = ClientSession()
 print("[INFO]: INITIALIZING ARQ CLIENT")
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
 
