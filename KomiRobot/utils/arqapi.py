c425186f1cb70fb52e33e2170ff8d12886dfa2b6
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-30 02:46:53 +0530

    Update arqapi.py

--- KomiRobot/utils/arqapi.py
+++ KomiRobot/utils/arqapi.py
@@ -18,8 +18,6 @@ ARQ_API_URL = "https://thearq.tech"
 
 # Aiohttp Client
 print("[INFO]: INITIALZING AIOHTTP SESSION")
-aiohttpsession = ClientSession()
-# ARQ Client
 print("[INFO]: INITIALIZING ARQ CLIENT")
 arq = ARQ(ARQ_API_URL, ARQ_API_KEY)
 
