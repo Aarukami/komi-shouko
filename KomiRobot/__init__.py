fc9adda5953b8c5642ff467724af3b88c630718a
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-30 02:41:05 +0530

    Update __init__.py

--- KomiRobot/__init__.py
+++ KomiRobot/__init__.py
@@ -260,9 +260,8 @@ pgram = Client("KuramaPyro", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 print("[INFO]: INITIALIZING AIOHTTP SESSION")
 # ARQ Client
-aiohttpsession = ClientSession()
 print("[INFO]: INITIALIZING ARQ CLIENT")
-arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY)
 
 ubot2 = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
 try:
