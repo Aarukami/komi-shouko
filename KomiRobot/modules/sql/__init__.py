51cc05497146570f6acb8477e802ff750977abb1
Blank <93943699+Blank-sama@users.noreply.github.com>
2023-08-24 13:03:51 +0530

    Update __init__.py

--- KomiRobot/modules/sql/__init__.py
+++ KomiRobot/modules/sql/__init__.py
@@ -9,7 +9,22 @@ def start() -> scoped_session:
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=True))
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import MySQLdb
 
+connection = MySQLdb.connect(
+  host="aws.connect.psdb.cloud",
+  user="fjhmmx7pe6dcla2966km",
+  passwd= "pscale_pw_q0pipgN1eW0hTkvX3Lts3IljZ3vFuV7DAKc8FBamZyc",
+  db= "komi",
+  autocommit = True,
+  ssl_mode = "VERIFY_IDENTITY",
+  ssl      = {
+    "ca": "/etc/ssl/cert.pem"
+  }
+)
 
 BASE = declarative_base()
-SESSION = start()
+SESSION = connection
