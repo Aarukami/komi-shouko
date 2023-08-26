74eb7cc948710a4b76fd6de8b29a890d1a01aca2
Blank <93943699+Blank-sama@users.noreply.github.com>
2023-08-26 18:50:40 +0530

    Update config.py

--- KomiRobot/config.py
+++ KomiRobot/config.py
@@ -76,7 +76,7 @@ class Config(object):
     TEMP_DOWNLOAD_DIRECTORY = ""
     HEROKU_APP_NAME= ""
     HEROKU_API_KEY = ""
-    REDIS_URL = " rediss://:qenym0pkRhftQdAIeT8Mzu5VkKESnp4Lredis-14131.c264.ap-south-1-1.ec2.cloud.redislabs.com:14131"
+    REDIS_URL = " redis://:qenym0pkRhftQdAIeT8Mzu5VkKESnp4Lredis-14131.c264.ap-south-1-1.ec2.cloud.redislabs.com:14131"
     SESSION_STRING =""
     SESSION_STRING = ""
     WELCOME_DELAY_KICK_SEC =""
